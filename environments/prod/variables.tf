8b999faead5a1325b27394b67ee011057acbecd7
Highbuilder08 <code.shadow08@gmail.com>
2026-08-19 12:48:36 +0900

    Add Bastion host for management access

--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -2,3 +2,8 @@ variable "admin_cidr" {
   description = "Administrator public IP CIDR allowed to access Bastion"
   type        = string
 }
+
+variable "ec2_key_name" {
+  description = "EC2 key pair name used for Bastion and private EC2 instances"
+  type        = string
+}
