de467aee85f39d96f6513b0b3aba1a8db37deba6
Highbuilder08 <code.shadow08@gmail.com>
2026-08-21 12:18:21 +0900

    Add dedicated three-tier network subnets

--- environments/prod/route.tf
+++ environments/prod/route.tf
@@ -48,3 +48,91 @@ resource "aws_route" "private_nat" {
   destination_cidr_block = "0.0.0.0/0"
   network_interface_id   = aws_instance.nat.primary_network_interface_id
 }
+
+# ---------------------------------------------------------
+# Web Tier Route Table
+# ---------------------------------------------------------
+
+resource "aws_route_table" "web_private" {
+  vpc_id = aws_vpc.pharmaflow.id
+
+  tags = {
+    Name        = "pharmaflow-web-private-rt"
+    Project     = "PharmaFlow"
+    Environment = "prod"
+    Tier        = "web"
+  }
+}
+
+resource "aws_route" "web_private_nat" {
+  route_table_id         = aws_route_table.web_private.id
+  destination_cidr_block = "0.0.0.0/0"
+  network_interface_id   = aws_instance.nat.primary_network_interface_id
+}
+
+resource "aws_route_table_association" "web_private_a" {
+  subnet_id      = aws_subnet.web_private_a.id
+  route_table_id = aws_route_table.web_private.id
+}
+
+resource "aws_route_table_association" "web_private_c" {
+  subnet_id      = aws_subnet.web_private_c.id
+  route_table_id = aws_route_table.web_private.id
+}
+
+# ---------------------------------------------------------
+# Application Tier Route Table
+# ---------------------------------------------------------
+
+resource "aws_route_table" "app_private" {
+  vpc_id = aws_vpc.pharmaflow.id
+
+  tags = {
+    Name        = "pharmaflow-app-private-rt"
+    Project     = "PharmaFlow"
+    Environment = "prod"
+    Tier        = "application"
+  }
+}
+
+resource "aws_route" "app_private_nat" {
+  route_table_id         = aws_route_table.app_private.id
+  destination_cidr_block = "0.0.0.0/0"
+  network_interface_id   = aws_instance.nat.primary_network_interface_id
+}
+
+resource "aws_route_table_association" "app_private_a" {
+  subnet_id      = aws_subnet.app_private_a.id
+  route_table_id = aws_route_table.app_private.id
+}
+
+resource "aws_route_table_association" "app_private_c" {
+  subnet_id      = aws_subnet.app_private_c.id
+  route_table_id = aws_route_table.app_private.id
+}
+
+# ---------------------------------------------------------
+# Database Tier Route Table
+# No Internet default route
+# ---------------------------------------------------------
+
+resource "aws_route_table" "db_private" {
+  vpc_id = aws_vpc.pharmaflow.id
+
+  tags = {
+    Name        = "pharmaflow-db-private-rt"
+    Project     = "PharmaFlow"
+    Environment = "prod"
+    Tier        = "database"
+  }
+}
+
+resource "aws_route_table_association" "db_private_a" {
+  subnet_id      = aws_subnet.db_private_a.id
+  route_table_id = aws_route_table.db_private.id
+}
+
+resource "aws_route_table_association" "db_private_c" {
+  subnet_id      = aws_subnet.db_private_c.id
+  route_table_id = aws_route_table.db_private.id
+}
--- environments/prod/subnet.tf
+++ environments/prod/subnet.tf
@@ -53,3 +53,105 @@ resource "aws_subnet" "private_c" {
     Type        = "private"
   }
 }
+
+# ---------------------------------------------------------
+# Web Tier Private Subnets
+# ---------------------------------------------------------
+
+resource "aws_subnet" "web_private_a" {
+  vpc_id                  = aws_vpc.pharmaflow.id
+  cidr_block              = "10.23.21.0/24"
+  availability_zone       = "ap-northeast-2a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "pharmaflow-web-private-a"
+    Project     = "PharmaFlow"
+    Environment = "prod"
+    Type        = "web-private"
+    Tier        = "web"
+  }
+}
+
+resource "aws_subnet" "web_private_c" {
+  vpc_id                  = aws_vpc.pharmaflow.id
+  cidr_block              = "10.23.22.0/24"
+  availability_zone       = "ap-northeast-2c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "pharmaflow-web-private-c"
+    Project     = "PharmaFlow"
+    Environment = "prod"
+    Type        = "web-private"
+    Tier        = "web"
+  }
+}
+
+# ---------------------------------------------------------
+# Application Tier Private Subnets
+# ---------------------------------------------------------
+
+resource "aws_subnet" "app_private_a" {
+  vpc_id                  = aws_vpc.pharmaflow.id
+  cidr_block              = "10.23.31.0/24"
+  availability_zone       = "ap-northeast-2a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "pharmaflow-app-private-a"
+    Project     = "PharmaFlow"
+    Environment = "prod"
+    Type        = "app-private"
+    Tier        = "application"
+  }
+}
+
+resource "aws_subnet" "app_private_c" {
+  vpc_id                  = aws_vpc.pharmaflow.id
+  cidr_block              = "10.23.32.0/24"
+  availability_zone       = "ap-northeast-2c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "pharmaflow-app-private-c"
+    Project     = "PharmaFlow"
+    Environment = "prod"
+    Type        = "app-private"
+    Tier        = "application"
+  }
+}
+
+# ---------------------------------------------------------
+# Database Tier Private Subnets
+# ---------------------------------------------------------
+
+resource "aws_subnet" "db_private_a" {
+  vpc_id                  = aws_vpc.pharmaflow.id
+  cidr_block              = "10.23.41.0/24"
+  availability_zone       = "ap-northeast-2a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "pharmaflow-db-private-a"
+    Project     = "PharmaFlow"
+    Environment = "prod"
+    Type        = "db-private"
+    Tier        = "database"
+  }
+}
+
+resource "aws_subnet" "db_private_c" {
+  vpc_id                  = aws_vpc.pharmaflow.id
+  cidr_block              = "10.23.42.0/24"
+  availability_zone       = "ap-northeast-2c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "pharmaflow-db-private-c"
+    Project     = "PharmaFlow"
+    Environment = "prod"
+    Type        = "db-private"
+    Tier        = "database"
+  }
+}
