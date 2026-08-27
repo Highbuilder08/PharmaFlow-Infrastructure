ce6ed826a780a1effc79ab096b0de874d2496aa5
Highbuilder08 <code.shadow08@gmail.com>
2026-08-27 12:12:25 +0900

    Add GitHub OIDC role for infrastructure CD

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket       = "pharmaflow-terraform-state-962450756907"
     key          = "prod/terraform.tfstate"
     region       = "ap-northeast-2"
-    profile      = "pharmaflow"
     encrypt      = true
     use_lockfile = true
   }
